626708aa2b0752f29e53e83a65e3d4edc2643c7f
MochilaCheia <paulovvieirag@gmail.com>
2022-02-12 19:05:34 -0300

    arquivo de funções com estruturas e recursos para o jogo e história

---
+++ componentes.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+def introducao():
+    sleep(4)
+    print('Olá, esse é o Adventure Game!\nUm jogo criado para ser como uma hitória em que suas escolhas vão te levar até em diversas aventuras e finais diferentes.')
+    sleep(3)
+    print('explore a história e veja os mais diversos finais se surpreendendo com suas diferenças e revelações.\nNão desista e divirta-se! bom jogo!')
+
+def final_incompleto():
+    sleep(4)
+    print('Você morreu.')
+    sleep(3)
+    print('Não desânime caro herói, continue tentando e tenho certeza que chegara a um final glorioso, surpresas te aguardam!')
+    sleep(3)
+    print('Vejo você logo!')
+
+def final_conclusao():
+    print('')
+
+def final_encerramento():
+    print('Uma realização de Paulo Vieira\nEm parceria com Silmara\n Com o apoio, ensino e paciência de Matheus')
+    print('Agradecimentos especiais a: ... ')
+
+if __name__ == '__main__':
+    pass
