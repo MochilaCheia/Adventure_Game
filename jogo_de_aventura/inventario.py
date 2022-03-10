3ba6e19bd350fddcbfd116a4dd041a51c9451a4f
MochilaCheia <paulovvieirag@gmail.com>
2022-03-10 16:36:04 -0300

    Segunda função de adicionar, tipo de inserir

--- jogo_de_aventura/inventario.py
+++ jogo_de_aventura/inventario.py
@@ -2,7 +2,7 @@ from time import sleep
 
 # FLAG: componentes do inventario
 
-def inventario_ato1():
+def inventario_adicionar_padrao():
     print('Você acessou seu inventário.')
     inventario_inicial = ['Vazio']
     print(f'Seu inventário está {inventario_inicial[0]}.')
@@ -27,5 +27,35 @@ def inventario_ato1():
     else:
         print(f'Seu inventário continuara {inventario_inicial[0]}.')
 
+def inventario_adicionar():
+    sleep(1)
+    print('Você acessou seu inventário.')
+    inventario_adicionar = ['Vazio']
+    sleep(1)
+    print(f'Seu inventário está {inventario_adicionar[0]}.')
+
+    sleep(1)
+    escolha = input('Deseja adicionar um item ao seu inventário?\n')
+
+    if escolha == 'Sim':
+
+        inventario_adicionar.pop()
+        sleep(1)
+        adicao = input('Digite o item que deseja adicionar ao seu inventario:\n')
+        inventario_adicionar.append(adicao)
+        sleep(1)
+        print(f'A {adicao} foi adicionado ao seu inventário.')
+        sleep(1)
+        pergunta = input('Deseja ver seu inventário?\n')
+
+        if pergunta == 'Sim':
+            print(f'No seu inventário tem: {inventario_adicionar[0]}.')
+
+        else:
+            print('Fechando inventário.')
+
+    else:
+        print(f'Seu inventário continuara {inventario_adicionar[0]}.')
+
 if __name__ == '__main__':
-    print(inventario_ato1())
+    print(inventario_adicionar())
