93ae6cc8339e0a2119af5825d4e18e351862dc2b
MochilaCheia <paulovvieirag@gmail.com>
2022-03-16 23:21:17 -0300

    Tratamento de entrada da resposta do usuário

--- jogo_de_aventura/inventario.py
+++ jogo_de_aventura/inventario.py
@@ -8,9 +8,9 @@ def inventario_adicionar_padrao():
     print(f'Seu inventário está {inventario_inicial[0]}.')
 
     escolha = input('Deseja adicionar a Espada flamejante ancestral ao seu inventário?\n')
-    escolha_tratada = escolha.title()
+    escolha_tratada = escolha.lower().strip()
 
-    if escolha_tratada == 'Sim':
+    if escolha_tratada == 'sim':
 
         inventario_inicial.pop()
         inventario_inicial.append('Espada flamejante ancestral')
@@ -18,9 +18,9 @@ def inventario_adicionar_padrao():
 
         sleep(1)
         pergunta = input('Deseja ver seu inventário?\n')
-        pergunta_tratada = pergunta.title()
+        pergunta_tratada = pergunta.lower().strip()
 
-        if pergunta_tratada == 'Sim':
+        if pergunta_tratada == 'sim':
             print(f'No seu inventário tem: {inventario_inicial[0]}.')
 
         else:
@@ -38,21 +38,22 @@ def inventario_adicionar():
 
     sleep(1)
     escolha = input('Deseja adicionar um item ao seu inventário?\n')
-    escolha_tratada = escolha.title()
+    escolha_tratada = escolha.lower().strip()
 
-    if escolha_tratada == 'Sim':
+    if escolha_tratada == 'sim':
 
         inventario_adicionar.pop()
         sleep(1)
         adicao = input('Digite o item que deseja adicionar ao seu inventario:\n')
         inventario_adicionar.append(adicao)
+        adicao_limpa = adicao.strip()
         sleep(1)
-        print(f'A {adicao} foi adicionado ao seu inventário.')
+        print(f'A {adicao_limpa} foi adicionado ao seu inventário.')
         sleep(1)
         pergunta = input('Deseja ver seu inventário?\n')
-        pergunta_tratada = pergunta.title()
+        pergunta_tratada = pergunta.lower().strip()
 
-        if pergunta_tratada == 'Sim':
+        if pergunta_tratada == 'sim':
             print(f'No seu inventário tem: {inventario_adicionar[0]}.')
 
         else:
@@ -62,4 +63,4 @@ def inventario_adicionar():
         print(f'Seu inventário continuara {inventario_adicionar[0]}.')
 
 if __name__ == '__main__':
-    print(inventario_adicionar())
+    pass
